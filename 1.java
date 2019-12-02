f6e3830d43ffaa558ec1a2b2d88880c78fec0be4
qi <2268618708@qq.com>
2019-12-02 15:04:09 +0800

    add 1.java

---
+++ 1.java
@@ -0,0 +1,21 @@
+﻿
+public class Person {
+	private String name;
+	private String hobby;
+	private String sex;
+	public int age;
+	
+	public Person(){
+		System.out.println("person()...");
+	}
+	public Person(String name){
+		this.name=name;
+		System.out.println("person("+name+")");
+	}
+	public void eat(){
+		System.out.println("eat...");
+	}
+	public void eat(String food){
+		System.out.println("eat..."+food);
+	}
+}
